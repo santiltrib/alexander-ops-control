a0038ebde1d2177b6e7563f799a284685888bcaf
Alex <santilwork@gmail.com>
2026-07-03 11:18:58 +0300

    Correct Polina traction KPI draft

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const DATA_URL = './data/current_backlog_2026-07-03.json';
-const ENCRYPTED_DATA_URL = './data/current_backlog_2026-07-03-v3.enc.json';
-const STORAGE_KEY = 'alexander-ops-cloud-skeleton-v0-12-daily-control-2026-07-03-v3';
+const ENCRYPTED_DATA_URL = './data/current_backlog_2026-07-03-v4.enc.json';
+const STORAGE_KEY = 'alexander-ops-cloud-skeleton-v0-13-daily-control-2026-07-03-v4';
 
 const statusOptions = [
   'new',
